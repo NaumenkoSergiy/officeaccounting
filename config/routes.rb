e6599fa99b67427cc00e65742cb84a16a31dcb7b
naumenko <mobilesoft@spaces.ru>
2014-11-25 10:00:18 +0200

    fixed destroy session route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'settings#index'
 
-  resources :sessions, only: [:new, :create, :destroy]
+  get 'destroy/sessions' => 'sessions#destroy'
+
+  resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create]
   resources :settings, only: [:index]
 end
