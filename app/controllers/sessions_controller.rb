9813b5c502bff5c0a2a78af24d98b4a8924406eb
victor <vitya.shmigol@gmail.com>
2014-12-09 11:29:58 +0200

    Login page: if I enter invalid credentials it reloads the page and email field is blank so every time I have to type it again. It should keep email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,16 @@
 class SessionsController < ApplicationController
   before_action :define_session_service
 
-  def new; end
+  def new
+  end
 
   def create
     @session_service.create_session params
 
     if current_user
-      if is_admin?
-        redirect_to admin_root_path
-      else
-        redirect_to root_path
-      end
+      redirect_to admin_root_path if is_admin?
     else
-      redirect_to new_session_path, flash: { error: 'Введені неправильні креденшіали' }
+      flash[:error] = 'Введені неправильні креденшіали'
     end
   end
 
