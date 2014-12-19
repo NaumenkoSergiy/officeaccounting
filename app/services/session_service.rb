885b0b91f1e0a92e82e7cb9b24d6f88aa8f5d883
victor <vitya.shmigol@gmail.com>
2014-12-19 13:54:27 +0200

    Змінити повідомлення про помилку при реєстрації

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -1,9 +1,13 @@
 class SessionService < BaseService
   def create_session data
     user = User.find_by(email: data[:email], password: data[:password])
-
-    if user && user.activated?
+    if user && !user.activated?
+      {error: 'Акаунт не активований! Перегляньте електронну пошту і перейдіть за посиланню для активації акаунта!'}
+    elsif user
       @session[:user_id] = user.id
+      {success: true}
+    else
+      {error: 'Введені неправильні дані'}
     end
   end
 end
