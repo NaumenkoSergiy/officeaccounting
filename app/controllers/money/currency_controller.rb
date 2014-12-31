371b7d8aea8f361739ecdc880cb20ac616ba12ba
victor <vitya.shmigol@gmail.com>
2014-12-31 15:37:31 +0200

    add service exchange rates

---
+++ app/controllers/money/currency_controller.rb
@@ -0,0 +1,7 @@
+module Money  
+  class CurrencyController < ApplicationController
+    def index
+      
+    end
+  end
+end
---
+++ app/controllers/money_controller.rb
@@ -0,0 +1,5 @@
+class MoneyController < ApplicationController
+
+  def index
+  end
+end
