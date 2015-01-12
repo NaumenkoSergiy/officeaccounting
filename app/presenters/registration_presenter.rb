846ac51f9da33ae73997515756ef21e766a6c539
naumenko <mobilesoft@spaces.ru>
2015-01-12 11:03:42 +0200

    fixed indentation

--- app/presenters/registration_presenter.rb
+++ app/presenters/registration_presenter.rb
@@ -2,6 +2,6 @@ class RegistrationPresenter < BasePresenter
   presents :registration
 
   def kveds
-    Kved.all.map{|k| k.section_number_name}.to_s.html_safe
+    Kved.select(:section, :number, :name).map(&:section_number_name).to_s.html_safe
   end
 end
