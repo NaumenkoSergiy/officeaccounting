8e9b2525eea0909ab2a29e18633fffabf63d44fb
victir <vitya.shmigol@gmail.com>
2015-01-23 13:32:56 +0200

    add jquery validations for forgot password

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -32,6 +32,54 @@ $(document).ready(function() {
     $(this).validate();
   });
 
+  $('#password_resets').validate({
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      error.insertBefore(element);
+    },
+    rules: {
+      email: {
+        required: true,
+        email: true,
+        pattern:  /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+      }
+    },
+    messages: {
+      email: {
+        required: 'Поле email має бути валідним',
+        email: 'Невірний формат'
+      }
+    }
+  });
+
+  $('.edit_user').validate({
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      error.insertBefore(element);
+    },
+    rules: {
+      "user[password]": {
+        required:  true,
+        minlength: 8,
+        maxlength: 32
+      },
+      "user[confirm_password]": {
+        required: true,
+        equalTo: "#user_password"
+      }
+    },
+    messages: {
+      "user[password]": {
+        required: 'Поле пароль не має бути бути порожнім',
+        minlength:  'Поле пароль має бути мінімум 8 символів'
+      },
+      "user[confirm_password]": {
+        required:  'Поле підтвердження пароля не має бути порожнім',
+        equalTo:  'Паролі не однакові'
+      }
+    }
+  });
+
   $('#new_session_form').submit(function(){
     if (!$(this).valid()) {
       return false
