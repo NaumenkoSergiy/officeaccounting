2807c21d05e003c26e6aa90c29131349eaff94a9
victir <vitya.shmigol@gmail.com>
2015-01-29 23:39:48 +0200

    add jquery validation for counterparties

--- app/assets/javascripts/purchases.js
+++ app/assets/javascripts/purchases.js
@@ -1,3 +1,29 @@
+function validCounterparty () {
+  $('#new_counterparty').validate({
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      error.insertBefore(element);
+    },
+    rules: {
+      "counterparty[name]": {
+        required: true
+      },
+      "counterparty[start_date]": {
+        required: true,
+        dpDate: true
+      }
+    },
+    messages: {
+      "counterparty[name]": {
+        required: 'поле не може бути пустим'
+      },
+      "counterparty[start_date]": {
+        required: 'поле не може бути пустим'
+      }
+    }
+  });
+}
+
 function openform () {
   $('#add_new_counterparty').click(function() {
     $(this).parents('.modal-header').next().find('form').toggle();
