25576cdd50947a474a901fdfc16ef81d9a6979d9
victir <vitya.shmigol@gmail.com>
2015-02-03 14:36:16 +0200

    add spec model Contract

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Contract, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation name' do
+    let(:company) { FactoryGirl.create(:company) }
+    let(:counterparty) { FactoryGirl.create(:counterparty) }
+    let!(:contract) { FactoryGirl.create(:contract, company_id: company.id, counterparty_id: counterparty.id) }
+
+    describe 'unvalid date validation' do
+      it 'has unvalid date' do
+        contract.date = ''
+        expect(contract).to be_invalid
+      end
+
+      it 'has unvalid date' do
+        contract.date = nil
+        expect(contract).to be_invalid
+      end
+    end
+
+    describe 'valid date validation' do
+      it 'has valid date' do
+        contract.date = Faker::Date.between(2.days.ago, Date.today)
+        expect(contract).to be_valid
+      end
+    end
+
+    describe 'unvalid number validation' do
+      it 'has unvalid number' do
+        contract.number = ''
+        expect(contract).to be_invalid
+      end
+
+      it 'has unvalid number' do
+        contract.number = nil
+        expect(contract).to be_invalid
+      end
+    end
+
+    describe 'valid number validation' do
+      it 'has valid number' do
+        contract.number = Faker::Number.number(8)
+        expect(contract).to be_valid
+      end
+    end
+
+    describe 'unvalid validity validation' do
+      it 'has unvalid validity' do
+        contract.validity = ''
+        expect(contract).to be_invalid
+      end
+
+      it 'has unvalid validity' do
+        contract.validity = nil
+        expect(contract).to be_invalid
+      end
+    end
+
+    describe 'valid validity validation' do
+      it 'has valid validity' do
+        contract.validity = Faker::Date.between(2.days.ago, Date.today)
+        expect(contract).to be_valid
+      end
+    end
+  end
 end
