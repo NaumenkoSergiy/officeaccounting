696b37d552ac0b8db88d7c8794625bccd6379328
victir <vitya.shmigol@gmail.com>
2015-02-03 16:05:34 +0200

    add spec

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,66 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Contract, :type => :model do
-  context 'validation name' do
-    let(:company) { FactoryGirl.create(:company) }
-    let(:counterparty) { FactoryGirl.create(:counterparty) }
-    let!(:contract) { FactoryGirl.create(:contract, company_id: company.id, counterparty_id: counterparty.id) }
 
-    describe 'unvalid date validation' do
-      it 'has unvalid date' do
-        contract.date = ''
-        expect(contract).to be_invalid
-      end
-
-      it 'has unvalid date' do
-        contract.date = nil
-        expect(contract).to be_invalid
-      end
-    end
-
-    describe 'valid date validation' do
-      it 'has valid date' do
-        contract.date = Faker::Date.between(2.days.ago, Date.today)
-        expect(contract).to be_valid
-      end
-    end
-
-    describe 'unvalid number validation' do
-      it 'has unvalid number' do
-        contract.number = ''
-        expect(contract).to be_invalid
-      end
-
-      it 'has unvalid number' do
-        contract.number = nil
-        expect(contract).to be_invalid
-      end
-    end
-
-    describe 'valid number validation' do
-      it 'has valid number' do
-        contract.number = Faker::Number.number(8)
-        expect(contract).to be_valid
-      end
-    end
-
-    describe 'unvalid validity validation' do
-      it 'has unvalid validity' do
-        contract.validity = ''
-        expect(contract).to be_invalid
-      end
-
-      it 'has unvalid validity' do
-        contract.validity = nil
-        expect(contract).to be_invalid
-      end
-    end
-
-    describe 'valid validity validation' do
-      it 'has valid validity' do
-        contract.validity = Faker::Date.between(2.days.ago, Date.today)
-        expect(contract).to be_valid
-      end
-    end
+  describe 'validation rules' do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:number) }
+    it { should validate_presence_of(:validity) }
+    it { should allow_value(Faker::Number.number(8)).for(:number) }
+    it { should_not allow_value('').for(:number) }
   end
 end
