b7f0c2450cabe70ca6ef18793d96e202e849771b
victir <vitya.shmigol@gmail.com>
2015-02-19 13:15:25 +0200

    add revenue page

--- app/models/money_register.rb
+++ app/models/money_register.rb
@@ -10,5 +10,6 @@ class MoneyRegister < ActiveRecord::Base
 
   validates :total, :type_document, :contract_id, :counterparty_id, presence: true
 
-  DOCUMENT_TYPE = [:payment_pr, :refund_b, :payments_l, :payments_c , :calculation, :payments_a, :calculation_t, :write_offs]
+  DOCUMENT_TYPE_COST = [:payment_pr, :refund_b, :payments_l, :payments_c , :calculation, :payments_a, :calculation_t, :write_offs]
+  DOCUMENT_TYPE_INCOME = [:payment_fb, :refund_s, :osrhoene_l, :other_revenues, :return_s]
 end
