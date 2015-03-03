b2e75f1185eab89015e7c3b30d4612c089b6fd87
victir <vitya.shmigol@gmail.com>
2015-03-03 12:21:53 +0200

    fix hard code

--- app/helpers/currencies_transactions_helper.rb
+++ app/helpers/currencies_transactions_helper.rb
@@ -1,16 +1,14 @@
 module CurrenciesTransactionsHelper
   
-  def type_of_transaction(page = '')
+  def render_type_of_transaction_link(page = '')
     if page == 'purchase'
-      link_to t('money.currences_tran.order_p_f_currency'), '#', class: 'btn  btn-primary btn-modal'
       link_to t('buttons.buy_currency'), '#', class: 'btn  btn-primary btn-modal'
     else
-      
       link_to t('buttons.currency_sale'), '#', class: 'btn  btn-primary btn-modal'
     end
   end
 
-  def type_of_order(page= '')
+  def render_type_of_order_link(page= '')
     if page == 'purchase'
       link_to t('money.currences_tran.order_p_f_currency'), '#', class: 'btn  btn-primary btn-modal'
     else
