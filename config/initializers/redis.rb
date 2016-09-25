3d01ef257adf32f0813a64fec51efa21451b443a
NaumenkoSergii <s.naumenko@active-bridge.com>
2016-09-25 19:31:28 +0300

    config redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-Redis.current = Redis.new(host: 'localhost', port: 6379)
+uri = URI.parse('redis://redistogo:a76eeb6adc6cc8d59c02ae539adcf9b4@viperfish.redistogo.com:11943/')
+Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
